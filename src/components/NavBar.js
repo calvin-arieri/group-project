8abdce63202f2a46f5aeee73533df00e1492f20d
Ronald Kerama <keramaronald11@gmail.com>
2023-04-25 13:11:32 +0300

    chnages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,20 @@
+import React from 'react'
+
 function Navbar() {
     return (
       <nav>
         <ul>
           <li>
-            <a href="/">Home </a>
+            <a href="/Home">Home </a>
           </li>
           <li>
             <a href="/about">About</a>
           </li>
           <li>
-            <a href="/contact">Contact Us </a>
+            <a href="/Contact Us">Contact Us </a>
           </li>
         </ul>
       </nav>
     );
   }
-  
+  export default Navbar;
