9462163e5784efce8f615664226cc3bbe01960f7
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-04-25 13:06:57 +0300

    added target

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Contact from './components/Contact';
 
 function App() {
-  return (
-    
-  );
+  return(
+    <div>
+      <Contact />
+    </div>
+  )
 }
 
 export default App;
